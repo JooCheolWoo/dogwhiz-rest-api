e3d64c436e3c8606c64dc2fcbac1d5a6d5512569
Pobilover <tkfkdal@naver.com>
2023-06-27 02:55:13 +0900

    restart

---
+++ src/main/java/com/galaxypoby/dogwhizrestapinew/DogwhizRestApiNewApplication.java
@@ -0,0 +1,13 @@
+package com.galaxypoby.dogwhizrestapinew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogwhizRestApiNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogwhizRestApiNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galaxypoby/dogwhizrestapinew/DogwhizRestApiNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxypoby.dogwhizrestapinew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogwhizRestApiNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
