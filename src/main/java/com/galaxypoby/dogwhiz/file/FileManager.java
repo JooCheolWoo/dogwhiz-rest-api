2d6532964ee92d82bd9a2746ff15c6cd5cb60a14
Pobilover <tkfkdal@naver.com>
2023-07-01 02:38:02 +0900

    123

--- src/main/java/com/galaxypoby/dogwhiz/file/FileManager.java
+++ src/main/java/com/galaxypoby/dogwhiz/file/FileManager.java
@@ -3,23 +3,41 @@ package com.galaxypoby.dogwhiz.file;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Date;
 
 @Component
 public class FileManager {
 
-    private final Path rootLocation = Paths.get("/home/galaxypoby/storage/dogwhiz-dev/");
+    private final Path rootLocation = Paths.get("https://dev.api.hellodogwhiz.com//home/galaxypoby/storage/dogwhiz-dev/");
 
     public void uploadFile(MultipartFile file) {
-        try {
-            Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename()),
-                    StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        try(
+                /* 맥일 경우
+                FileOutputStream fos = new FileOutputStream(
+                    "/tmp/" + file.getOriginalFilename() + new Date());
+                */
+
+                // 윈도우일 경우
+                FileOutputStream fos = new FileOutputStream(
+                        "rootLocation" + file.getOriginalFilename() + new Date());
+                InputStream is = file.getInputStream();
+        ){
+            int readCount = 0;
+            byte[] buffer = new byte[1024];
+            int i = 0;
+            while((readCount = is.read(buffer)) != -1){
+                fos.write(buffer, 0, readCount);
+                System.out.println(i++);
+            }
+        }catch(Exception ex){
+            throw new RuntimeException("file Save Error");
         }
     }
 }
