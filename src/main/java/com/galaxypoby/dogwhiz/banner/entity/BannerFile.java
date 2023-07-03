5c5173e9c20e6685746e3b6f6d34365c8268e2e1
JooCheolWoo <tkfkdal@naver.com>
2023-07-03 16:02:23 +0900

    123

--- src/main/java/com/galaxypoby/dogwhiz/banner/entity/BannerFile.java
+++ src/main/java/com/galaxypoby/dogwhiz/banner/entity/BannerFile.java
@@ -24,7 +24,7 @@ public class BannerFile {
     private Long id;
 
     @JsonIgnore
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @MapsId
     @JoinColumn(name = "banner_id")
     private Banner banner;
