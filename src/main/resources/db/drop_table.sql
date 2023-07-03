7981adccd160b0ba6fe38ac686156655a810ab1e
JooCheolWoo <tkfkdal@naver.com>
2023-07-03 14:54:51 +0900

    123123

--- src/main/resources/db/drop_table.sql
+++ src/main/resources/db/drop_table.sql
@@ -5,9 +5,10 @@ DROP TABLE IF EXISTS `members_roles`;
 DROP TABLE IF EXISTS `roles_permissions`;
 DROP TABLE IF EXISTS `role`;
 DROP TABLE IF EXISTS `permission`;
-DROP TABLE IF EXISTS `member`;
 
 DROP TABLE IF EXISTS `banner_file`;
 DROP TABLE IF EXISTS `banner`;
+
+DROP TABLE IF EXISTS `member`;
 
 
