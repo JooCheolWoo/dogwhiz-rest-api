48253037fa32280371e64bf1771a35a296663274
Pobilover <tkfkdal@naver.com>
2023-07-06 21:16:02 +0900

    sda

--- src/main/java/com/galaxypoby/dogwhiz/login/dto/ResponseLoginDto.java
+++ src/main/java/com/galaxypoby/dogwhiz/login/dto/ResponseLoginDto.java
@@ -2,15 +2,18 @@ package com.galaxypoby.dogwhiz.login.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.galaxypoby.dogwhiz.member.entity.MemberImage;
+import com.galaxypoby.dogwhiz.member.entity.Role;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 public class ResponseLoginDto {
     private Long id;
     private String email;
     private String nickname;
+    private Set<Role> roles;
     private String loginIp;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime lastLoginDate;
