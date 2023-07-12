114cd36f5142dc9f201180c33190c31c39077cf3
Pobilover <tkfkdal@naver.com>
2023-07-13 02:33:47 +0900

    no message

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 APP_NAME="dogwhiz-rest-api"
 
+PROFILE=$1
+
 cd /home/galaxypoby/projects/${APP_NAME}
 git reset
 git checkout .
 git clean -fdx
-git pull
 
-PROFILE=$1
+if [ "$PROFILE" == "dev" ]; then
+    echo "git pull from origin dev"
+    git pull origin dev
+elif [ "$PROFILE" == "prod" ]; then
+    echo "git pull from origin main"
+    git pull origin main
+else
+    echo "Invalid PROFILE: $PROFILE. Please use 'dev' or 'prod'."
+    exit 1
+fi
+
 
 # execute docker build
 /bin/bash build-docker.sh $PROFILE
-
-
 
