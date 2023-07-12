9ab41192c4f24dff25e8368f7ee2851b30a05d01
Pobilover <tkfkdal@naver.com>
2023-07-13 05:48:02 +0900

    111

--- src/main/java/com/galaxypoby/dogwhiz/config/WebConfig.java
+++ src/main/java/com/galaxypoby/dogwhiz/config/WebConfig.java
@@ -14,10 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true)
-                .allowedOrigins("https://dev.front.hellodogwhiz.com"
-                        , "http://localhost:3000"
-                        , "https://w6gqmm-3000.csb.app")
+                .allowedOrigins("*")
+//                .allowCredentials(true)
+//                .allowedOrigins("https://dev.front.hellodogwhiz.com"
+//                        , "http://localhost:3000"
+//                        , "https://w6gqmm-3000.csb.app")
                 .allowedMethods("*")
                 .maxAge(3000);
     }
