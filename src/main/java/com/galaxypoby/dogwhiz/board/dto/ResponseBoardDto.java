9ff0e4606e17a972c22bf60fc4129a0e14c66528
Pobilover <tkfkdal@naver.com>
2023-07-16 09:36:03 +0900

    11

--- src/main/java/com/galaxypoby/dogwhiz/board/dto/ResponseBoardDto.java
+++ src/main/java/com/galaxypoby/dogwhiz/board/dto/ResponseBoardDto.java
@@ -25,11 +25,8 @@ public class ResponseBoardDto {
         private String content;
         private Long likeCount;
         private Long viewCount;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy'.'MM'.'dd HH:mm:ss")
         private LocalDateTime createdAt;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy'.'MM'.'dd HH:mm:ss")
         private LocalDateTime updatedAt;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy'.'MM'.'dd HH:mm:ss")
         private LocalDateTime deletedAt;
 
         public void setCategory(String category) {
@@ -54,7 +51,6 @@ public class ResponseBoardDto {
         private String content;
         private Long likeCount;
         private Long viewCount;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy'.'MM'.'dd HH:mm:ss")
         private LocalDateTime createdAt;
 
         public void setCategory(String category) {
